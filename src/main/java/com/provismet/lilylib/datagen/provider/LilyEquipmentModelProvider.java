664453ef9c5bb426095dbf5ef78c753d46728f9b
Provismet <17149901+Provismet@users.noreply.github.com>
2024-11-27 19:23:46 +0000

    Create equipment model datagen provider.

---
+++ src/main/java/com/provismet/lilylib/datagen/provider/LilyEquipmentModelProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024 Provismet
+ *
+ * See https://github.com/Provismet/LilyLib/blob/1.21/LICENSE for the full license.
+ */
+
+package com.provismet.lilylib.datagen.provider;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.data.DataOutput;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.DataWriter;
+import net.minecraft.item.equipment.EquipmentModel;
+import net.minecraft.util.Identifier;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+public abstract class LilyEquipmentModelProvider implements DataProvider {
+    private final DataOutput.PathResolver pathResolver;
+
+    public LilyEquipmentModelProvider (FabricDataOutput output) {
+        this.pathResolver = output.getResolver(DataOutput.OutputType.RESOURCE_PACK, "models/equipment");
+    }
+
+    @Override
+    public CompletableFuture<?> run (DataWriter writer) {
+        Map<Identifier, EquipmentModel> map = new HashMap<>();
+        this.generate(map::putIfAbsent);
+
+        return DataProvider.writeAllToPath(writer, EquipmentModel.CODEC, this.pathResolver, map);
+    }
+
+    @Override
+    public String getName () {
+        return "Equipment";
+    }
+
+    protected abstract void generate (BiConsumer<Identifier, EquipmentModel> consumer);
+
+    protected EquipmentModel buildHumanoid (Identifier modelId) {
+        return EquipmentModel.builder().addHumanoidLayers(modelId).build();
+    }
+
+    protected EquipmentModel buildHumanoidAndHorse (Identifier modelId) {
+        return EquipmentModel.builder()
+            .addHumanoidLayers(modelId)
+            .addLayers(EquipmentModel.LayerType.HORSE_BODY, EquipmentModel.Layer.createDyeableLeather(modelId, false))
+            .build();
+    }
+}
