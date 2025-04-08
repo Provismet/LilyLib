276eeb103d81bdb3acc41aa287236b26c5e83410
Provismet <17149901+Provismet@users.noreply.github.com>
2025-04-08 17:53:59 +0100

    Rework json builder and add reader.

---
+++ src/main/java/com/provismet/lilylib/util/json/JsonBuilder.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2024 Provismet
+ * 
+ * See https://github.com/Provismet/LilyLib/blob/1.21/LICENSE for the full license.
+ */
+
+package com.provismet.lilylib.util.json;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.Arrays;
+
+/**
+ * Builds JSON-formatted strings. This is a wrapper of JsonObject.
+ */
+public class JsonBuilder {
+    private final JsonObject json = new JsonObject();
+
+    public JsonBuilder () {
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     *
+     * @param key Key
+     * @param value Value
+     * @return this
+     */
+    public JsonBuilder append (String key, String value) {
+        this.json.addProperty(key, value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     *
+     * @param key Key
+     * @param value Value
+     * @return this
+     */
+    public JsonBuilder append (String key, Number value) {
+        this.json.addProperty(key, value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     *
+     * @param key Key
+     * @param value Value
+     * @return this
+     */
+    public JsonBuilder append (String key, boolean value) {
+        this.json.addProperty(key, value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     *
+     * @param key Key
+     * @param value Value
+     * @return this
+     */
+    public JsonBuilder append (String key, JsonBuilder value) {
+        this.json.add(key, value.json);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     *
+     * @param key Key
+     * @param value Value
+     * @return this
+     */
+    public JsonBuilder append (String key, JsonElement value) {
+        this.json.add(key, value);
+        return this;
+    }
+
+    /**
+     * <p> Creates an entire array from an iterable of String values. </p>
+     * 
+     * <p> This function encapsulates the starting and closing of the array. </p>
+     * 
+     * @param key The key of the key-value pair.
+     * @param values The values of the array.
+     * @return this
+     */
+    public JsonBuilder appendArray (String key, Iterable<String> values) {
+        JsonArray array = new JsonArray();
+        for (String value : values) {
+            array.add(value);
+        }
+        this.json.add(key, array);
+        return this;
+    }
+
+    /**
+     * <p> Creates an entire array from an undefined number of String arguments. </p>
+     * 
+     * <p> Uses the iterable variant as its implementation. </p>
+     * 
+     * @param key The key of the array.
+     * @param values The contents of the array.
+     * @return this
+     */
+    public JsonBuilder appendArray (String key, String... values) {
+        return this.appendArray(key, Arrays.stream(values).toList());
+    }
+
+    /**
+     * <p> Builds and returns the current String held by the internal StringBuilder. </p>
+     * 
+     * <p> Use this to output the final JSON string. </p>
+     * 
+     * @return The current string representation of the JSON object.
+     */
+    @Override
+    public String toString () {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(this.json);
+    }
+
+    /**
+     * @return The internal json held by this builder.
+     */
+    public JsonObject getJson () {
+        return this.json;
+    }
+}
---
+++ src/main/java/com/provismet/lilylib/util/json/JsonReader.java
@@ -0,0 +1,93 @@
+package com.provismet.lilylib.util.json;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class JsonReader {
+    private final JsonObject json;
+
+    public JsonReader (JsonObject json) {
+        this.json = json;
+    }
+
+    @Nullable
+    public static JsonReader file (File file) throws FileNotFoundException {
+        JsonElement element = JsonParser.parseReader(new FileReader(file));
+        if (element instanceof JsonObject json) {
+            return new JsonReader(json);
+        }
+        return null;
+    }
+
+    public Optional<Integer> getInteger (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsInt);
+    }
+
+    public Optional<Double> getDouble (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsDouble);
+    }
+
+    public Optional<Float> getFloat (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsFloat);
+    }
+
+    public Optional<Short> getShort (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsShort);
+    }
+
+    public Optional<Long> getLong (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsLong);
+    }
+
+    public Optional<Byte> getByte (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsByte);
+    }
+
+    public Optional<Boolean> getBoolean (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsBoolean);
+    }
+
+    public Optional<String> getString (String key) {
+        return this.getGeneric(key, JsonPrimitive::getAsString);
+    }
+
+    public Optional<JsonArray> getArray (String key) {
+        return this.get(key).map(element -> {
+            if (element instanceof JsonArray array) return array;
+            return null;
+        });
+    }
+
+    public Optional<JsonPrimitive> getPrimitive (String key) {
+        return this.get(key).map(element -> {
+            if (element instanceof JsonPrimitive primitive) return primitive;
+            return null;
+        });
+    }
+
+    public Optional<JsonElement> get (String key) {
+        if (this.json.has(key)) {
+            return Optional.of(this.json.get(key));
+        }
+        return Optional.empty();
+    }
+
+    public <T> Optional<T> getGeneric (String key, Function<JsonPrimitive, T> function) {
+        try {
+            return this.getPrimitive(key).map(function);
+        }
+        catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}
