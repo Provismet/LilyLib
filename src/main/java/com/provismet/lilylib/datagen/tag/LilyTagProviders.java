b67257a6722e3a12f9627f4e508b1b5465c95f1f
Provismet <17149901+Provismet@users.noreply.github.com>
2025-10-12 02:25:30 +0100

    Add enchantment tag provider.

--- src/main/java/com/provismet/lilylib/datagen/tag/LilyTagProviders.java
+++ src/main/java/com/provismet/lilylib/datagen/tag/LilyTagProviders.java
@@ -1,16 +1,21 @@
 /*
- * Copyright (C) 2024 Provismet
+ * Copyright (C) 2024-2025 Provismet
  *
- * See https://github.com/Provismet/LilyLib/blob/1.21/LICENSE for the full license.
+ * See https://github.com/Provismet/LilyLib/blob/1.21.10/LICENSE for the full license.
  */
 
 package com.provismet.lilylib.datagen.tag;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.data.tag.ProvidedTagBuilder;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.damage.DamageType;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -18,6 +23,12 @@ public abstract class LilyTagProviders {
     public static abstract class LilyDamageTypeTagProvider extends FabricTagProvider<DamageType> {
         public LilyDamageTypeTagProvider (FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
             super(output, RegistryKeys.DAMAGE_TYPE, registriesFuture);
+        }
+    }
+
+    public static abstract class LilyEnchantmentTagProvider extends FabricTagProvider<Enchantment> {
+        public LilyEnchantmentTagProvider (FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, RegistryKeys.ENCHANTMENT, registriesFuture);
         }
     }
 }
